5f235e6b15c8fe0acfc281ede1e12d842e93843a
feli <killerkralle@gmail.com>
2019-07-27 16:12:01 +0200

    27.07. Implants IN PROGRESS

--- Assets/MonsterLinkerAssets/Scripts/Arena/ImplantHandler.cs
+++ Assets/MonsterLinkerAssets/Scripts/Arena/ImplantHandler.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class ImplantHandler : MonoBehaviour
 {
     [Header("For Rising Rage")]
+    [Tooltip("Save of the RP count at the start of each attack round")]
     public float PlayerRPatAttackStart;
     [Tooltip("% more BaseDmg for one RP")]
     public float OneRP_Multiplier;
 
     [Header("For Unleashed Mode")]
     public bool UMactivated;
+    public int UMrounds;
+    public float UM_oneTimeHeal_Value;
+    public float UM_DMGDealt_Multiplier;
+    public float UM_DMGTaken_Reduction;
 
     [Header("For Super Feral Art")]
     public bool SFAused;
@@ -22,8 +27,22 @@ public class ImplantHandler : MonoBehaviour
     //check if SFA or UM have been used
     //check the rounds UM has been active
     //check modifiers of the implants
-    //one dmg modifier in baeffects only, it is set by the current implant?
+    //one dmg modifier in baeffects only, it is set by the current implant?    
 
+    public void ResetCounters()
+    {
+        PlayerRPatAttackStart = 0f;
+        UMactivated = false;
+        UMrounds = 0;
+        SFAused = false;
+        QTEmissed = false;
+    }
+
+    public float RPMultiplier()
+    {
+        float value = (OneRP_Multiplier * PlayerRPatAttackStart);
+        return value;
+    }
 
 
 }
